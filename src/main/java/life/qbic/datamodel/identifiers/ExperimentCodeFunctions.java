6b29aeac1786b9773a04e4a41f60d57cc20cad4a
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2018-09-03 11:26:09 +0200

    add functions to create identifiers for experiments

--- src/main/java/life/qbic/datamodel/identifiers/ExperimentCodeFunctions.java
+++ src/main/java/life/qbic/datamodel/identifiers/ExperimentCodeFunctions.java
@@ -9,20 +9,33 @@ public class ExperimentCodeFunctions {
 
   /**
    * returns -1 if code doesn't fit standard or integer can't be parsed
+   * 
    * @param project
    * @param code
    * @return
    */
-  public static int getNumSuffixOfExperimentCode(String project, String code) {
-    String[] split = code.split(project + "E");
-    int num = -1;
-    if (code.startsWith(project + "E") && split.length > 1) {
-      try {
-        num = Integer.parseInt(split[1]);
-      } catch (Exception e2) {
-        logger.warn("could not parse id of experiment" + code);
-      }
-    }
-    return num;
+//  public static int getNumSuffixOfExperimentCode(String project, String code) {
+//    String[] split = code.split(project + "E");
+//    int num = -1;
+//    if (code.startsWith(project + "E") && split.length > 1) {
+//      try {
+//        num = Integer.parseInt(split[1]);
+//      } catch (Exception e2) {
+//        logger.warn("could not parse id of experiment" + code);
+//      }
+//    }
+//    return num;
+//  }
+
+  public static String getInfoExperimentID(String space, String project) {
+    return "/" + space + "/" + project + "/" + project + "_INFO";
+  }
+
+  public static String getExperimentIdentifier(String space, String project, String exp) {
+    return "/" + space + "/" + project + "/" + exp;
+  }
+
+  public static String getExperimentIdentifier(String space, String project, int expIndex) {
+    return getExperimentIdentifier(space, project, project + Integer.toString(expIndex));
   }
 }
