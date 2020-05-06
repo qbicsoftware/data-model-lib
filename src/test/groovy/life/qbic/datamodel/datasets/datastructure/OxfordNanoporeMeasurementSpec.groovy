7e27a900ead5f1aa27bae5fa64bd152d5565a59a
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-05-06 15:22:43 +0200

    Provide test for pooled sample raw data API

--- src/test/groovy/life/qbic/datamodel/datasets/datastructure/OxfordNanoporeMeasurementSpec.groovy
+++ src/test/groovy/life/qbic/datamodel/datasets/datastructure/OxfordNanoporeMeasurementSpec.groovy
@@ -2,7 +2,6 @@ package life.qbic.datamodel.datasets.datastructure
 
 import life.qbic.datamodel.datasets.Experiment
 import life.qbic.datamodel.datasets.OxfordNanoporeMeasurement
-import life.qbic.datamodel.datasets.datastructure.files.DataFile
 import life.qbic.datamodel.datasets.datastructure.files.nanopore.Fast5File
 import life.qbic.datamodel.datasets.datastructure.files.nanopore.FastQFile
 import life.qbic.datamodel.datasets.datastructure.folders.DataFolder
@@ -32,6 +31,10 @@ class OxfordNanoporeMeasurementSpec extends Specification {
     Fast5Folder fast5PooledFolder
     @Shared
     FastQFolder fastQPooledFolder
+    @Shared
+    Fast5Folder otherfast5PooledFolder
+    @Shared
+    FastQFolder otherfastQPooledFolder
 
     def setupSpec() {
         def fast5File = Fast5File.create("test.fast5", "root/test.fast5")
@@ -43,6 +46,8 @@ class OxfordNanoporeMeasurementSpec extends Specification {
         // Content for the pooled samples
         fast5PooledFolder = Fast5Folder.create("QTEST001AE_test", "root/QTEST001AE_test", [fast5File])
         fastQPooledFolder = FastQFolder.create("QTEST001AE_test", "root/QTEST001AE_test", [fastQFile])
+        otherfast5PooledFolder = Fast5Folder.create("QD00M001AE_test", "root/QD00M001AE_test", [fast5File])
+        otherfastQPooledFolder = FastQFolder.create("QD00M001AE_test", "root/QD00M001AE_test", [fastQFile])
     }
 
     def "create simple measurement successfully"() {
@@ -69,10 +74,10 @@ class OxfordNanoporeMeasurementSpec extends Specification {
 
     def "create pooled sample measurement successfully"() {
         given:
-        final def pooledFast5PassFolder = Fast5PassFolder.create("root/fast5_pass", [fast5PooledFolder])
-        final def pooledFast5FailedFolder = Fast5FailFolder.create("root/fast5_fail", [fast5PooledFolder])
-        final def pooledFastQPassFolder = FastQPassFolder.create("root/fastq_pass", [fastQPooledFolder])
-        final def pooledFastQFailedFolder = FastQFailFolder.create("root/fastq_fail", [fastQPooledFolder])
+        final def pooledFast5PassFolder = Fast5PassFolder.create("root/fast5_pass", [fast5PooledFolder, otherfast5PooledFolder])
+        final def pooledFast5FailedFolder = Fast5FailFolder.create("root/fast5_fail", [fast5PooledFolder, otherfast5PooledFolder])
+        final def pooledFastQPassFolder = FastQPassFolder.create("root/fastq_pass", [fastQPooledFolder, otherfastQPooledFolder])
+        final def pooledFastQFailedFolder = FastQFailFolder.create("root/fastq_fail", [fastQPooledFolder, otherfastQPooledFolder])
 
         final def measurement = OxfordNanoporeMeasurement.create(
                 "20200219_1107_1-E3-H3_PAE26974_454b8dc6",
@@ -87,8 +92,9 @@ class OxfordNanoporeMeasurementSpec extends Specification {
         def result = measurement.getRawDataPerSample(mockedExperiment)
 
         then:
-        assert result.size() == 1
+        assert result.size() == 2
         assert result.get("QTEST001AE").get("fast5fail") instanceof DataFolder
+        assert result.get("QD00M001AE").get("fast5fail") instanceof DataFolder
     }
 
 
