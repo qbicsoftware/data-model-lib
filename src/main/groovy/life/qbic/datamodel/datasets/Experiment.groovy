e9608badef5c908a066f9ef3eddb600678a5c548
Sven F <sven.fillinger@mailbox.org>
2020-05-11 11:26:58 +0200

    Refactor JavaDoc
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>

--- src/main/groovy/life/qbic/datamodel/datasets/Experiment.groovy
+++ src/main/groovy/life/qbic/datamodel/datasets/Experiment.groovy
@@ -3,7 +3,7 @@ package life.qbic.datamodel.datasets
 interface Experiment {
 
     /**
-     * Returns the QBiC sample Id that is associated with this Oxford Nanopre Experiment.
+     * Returns the QBiC sample Id that is associated with this Experiment.
      *
      * The sample id currently is encoded within the root folder's file name
      */
