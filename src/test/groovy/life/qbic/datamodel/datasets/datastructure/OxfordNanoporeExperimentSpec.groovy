95a4a5ddb54263a2e85cb9b92d2ad6a6953ccbb9
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-06-30 14:06:50 +0200

    Test OxfordNanoporeExperiment is created correctly for pooled data.
    Create a simple pooled dataset. The test creates a nanopore experiment from the given json file.
    It then verifies that 2 measurements are contained in the created experiment.
    Also the number of log files for the first measurement is tested.

--- src/test/groovy/life/qbic/datamodel/datasets/datastructure/OxfordNanoporeExperimentSpec.groovy
+++ src/test/groovy/life/qbic/datamodel/datasets/datastructure/OxfordNanoporeExperimentSpec.groovy
@@ -19,9 +19,15 @@ class OxfordNanoporeExperimentSpec extends Specification {
     @Shared
     Map minimalWorkingSimpleDataStructure
 
+    @Shared
+    Map minimalWorkingPooledDataStructure
+
     def setupSpec() {
-        InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("valid-example.json")
-        minimalWorkingSimpleDataStructure = (Map) new JsonSlurper().parse(stream)
+        InputStream singleSampleStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("valid-example.json")
+        minimalWorkingSimpleDataStructure = (Map) new JsonSlurper().parse(singleSampleStream)
+
+        InputStream pooledSampleStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("valid-example-pooled.json")
+        minimalWorkingPooledDataStructure = (Map) new JsonSlurper().parse(pooledSampleStream)
     }
 
     def "Create simple sample Oxford Nanopore experiment successfully"() {
@@ -35,7 +41,22 @@ class OxfordNanoporeExperimentSpec extends Specification {
         then:
         assert experiment.sampleCode == "QABCD001AB"
         assert measurements.size() == 1
+    }
+
+    def "Create a simple pooled Oxford Nanopore experiment successfully"() {
+        given:
+        final def example = minimalWorkingPooledDataStructure
+
+        when:
+        final def experiment = OxfordNanoporeExperiment.create(example)
+        final def measurements = experiment.getMeasurements()
+
+        then:
+        assert experiment.sampleCode == "QABCD001AB"
+        assert measurements.size() == 1
         assert measurements.get(0).logFiles.size() == 8
+        // two samples in measurement
+        assert measurements.get(0).getRawDataPerSample(experiment).keySet().size() == 2
 
     }
 }
