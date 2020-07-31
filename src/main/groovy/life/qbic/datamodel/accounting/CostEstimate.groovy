aaa3a2a5baeed15abd0ecfd8db7e7a24abd2c131
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-07-31 10:52:58 +0200

    Add CostEstimate class
    This commit adds a CostEstimate class.
     * During project planning a cost estimate is often required to list expected costs.
     * This estimate is not intended to be legally binding.
     * It provides an overview over the project scope and the costs associated with it.

---
+++ src/main/groovy/life/qbic/datamodel/accounting/CostEstimate.groovy
@@ -0,0 +1,23 @@
+package life.qbic.datamodel.accounting
+
+import life.qbic.datamodel.persons.Person
+import life.qbic.datamodel.projects.ProjectInfo
+
+/**
+ * A cost estimate for a project
+ *
+ * During project planning a cost estimate is often required to list expected costs.
+ * This estimate is not intended to be legally binding. It provides an overview over the project
+ * scope and the costs associated with it.
+ *
+ * @since: 1.9.0
+ * @author: Tobias Koch
+ *
+ * * */
+class CostEstimate {
+    Date modificationDate
+    Person customer
+    ProjectInfo projectInfo
+    List<CostItem> items
+    double totalPrice
+}
