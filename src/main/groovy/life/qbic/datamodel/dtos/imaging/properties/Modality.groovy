78b038972ee4bde50e3b5a3d1a99865f902e0b67
Sven F <sven.fillinger@mailbox.org>
2020-08-12 15:22:37 +0200

    Add modality enum class (#39)
    The modality DTO class provides an enumeration for different imaging modalities.

--- src/main/groovy/life/qbic/datamodel/dtos/imaging/properties/Modality.groovy
+++ src/main/groovy/life/qbic/datamodel/dtos/imaging/properties/Modality.groovy
@@ -1,5 +1,53 @@
 package life.qbic.datamodel.dtos.imaging.properties
 
+/**
+ * Enumeration of different microscopy and spectroscopy modalities.
+ *
+ * This enum class should be used, whenever a imaging modality needs
+ * to be described.
+ *
+ * Every modality needs to have a meaningful and descriptive label.
+ * Abbreviations are discouraged.
+ */
 enum Modality {
+
+  ANISOTROPY("Anisotropic microscopy"),
+  CLSM("Confocal laser scanning microscopy"),
+  DSTOR("Direct stochastic optical reconstruction microscopy"),
+  FLCS("Fluorescence lifetime correlation spectroscopy"),
+  FLIM("Fluorescence lifetime imaging microscopy"),
+  SPECTROSCOPY("Spectroscopy"),
+  SPT_PALM("Photo-activated localization microscopy"),
+  SEM("Scanning electron microscopy")
+
+  /**
+   * Contains the String representation of a modality item.
+   */
+  private String value
+
+  private Modality(String value) {
+    this.value = value
+  }
+
+  /**
+   * The string representation of the modality item.
+   * @return The value of the modality item, which is its declarative String representation.
+   */
+  String getValue() {
+    return this.value
+  }
+
+  /**
+   * The <code>toString()</code> method is overridden to return the String
+   * representation of a modality item. This is equivalent
+   * to the <code>getValue()</code> return value.
+   *
+   * @see #getValue()
+   * @return The String representation of the modality item
+   */
+  @Override
+  String toString() {
+    return this.getValue()
+  }
 
 }
