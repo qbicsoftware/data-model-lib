3d89888152cac8f496603cebc9c0a98af5065679
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2020-08-12 11:17:13 +0200

    add electrone microscopy properties

--- src/main/groovy/life/qbic/datamodel/dtos/imaging/parameters/electronmicroscopy/ElectronMicroscopyParameters.groovy
+++ src/main/groovy/life/qbic/datamodel/dtos/imaging/parameters/electronmicroscopy/ElectronMicroscopyParameters.groovy
@@ -1,12 +1,35 @@
 package life.qbic.datamodel.dtos.imaging.parameters.electronmicroscopy
 
+import life.qbic.datamodel.dtos.imaging.properties.Voltage
+
 /**
- * <class short description - 1 Line!>
+ * Describes the properties of electron microscopy
  *
- * <More detailed description - When to use, what it solves, etc.>
+ * This class serves as DTO and contains information about
+ * general electron microscope parameters during an image analysis.
+ *
+ * This class shall be extended, if you need to add more technology-specific
+ * properties.
  *
  * @author Sven Fillinger
- * @since <versiontag>
+ * @since 1.0
  */
 class ElectronMicroscopyParameters {
+
+    /**
+     * Voltage used to accelerate electrons.
+     * Applied between cathode and electrode in the acceleration tube
+     */
+    private final Voltage acceleratingVoltage
+
+    /**
+     * Shift of focus of the objective lens.
+     * It is used to observe 'Fresnel fringes', for taking lattice image or structure images.
+     */
+    private final Float targetDefocus
+
+    /**
+     * The accumulated amount of electrons transmitting the materials
+     */
+    private final Float cumulativeElectronDose
 }
