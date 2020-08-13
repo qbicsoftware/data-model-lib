1301b8c86e12cf24cbe9692ab8da77aeab5301b1
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-08-13 10:24:39 +0200

    Add dto for instrument

--- src/main/groovy/life/qbic/datamodel/dtos/imaging/Instrument.groovy
+++ src/main/groovy/life/qbic/datamodel/dtos/imaging/Instrument.groovy
@@ -1,12 +1,72 @@
 package life.qbic.datamodel.dtos.imaging
 
 /**
- * <class short description - 1 Line!>
+ * DTO that describes an instrument in the imaging context
  *
- * <More detailed description - When to use, what it solves, etc.>
+ * This class shall be used to describe some basic
+ * properties of an instrument that has been used to create an image
+ * during for example a microscopy or tomography analysis.
  *
  * @author Sven Fillinger
- * @since <versiontag>
+ * @since 1.10.0
  */
 class Instrument {
+
+  /**
+   * The manufacturer of the instrument.
+   * Examples are 'Leica', 'Zeiss' and many more.
+   */
+  private final String manufacturer
+
+  /**
+   * The model type of the instrument.
+   */
+  private final String model
+
+  /**
+   * The serial number of the instrument.
+   */
+  private final String serialNumber
+
+  /**
+   * The location of the instrument.
+   *
+   * @see: #Location
+   */
+  private final Location location
+
+  /**
+   * The type of the instrument.
+   * Example: 'Confocal Microscope'.
+   */
+  private final String type
+
+  /**
+   * A more detailed description of the
+   * hardware setup.
+   *
+   * @see: #ImagingHardware
+   */
+  private final ImagingHardware hardware
+
+  /**
+   * Returns a deep copy of the imaging hardware.
+   *
+   * @see: #ImagingHardware
+   * @return The imaging hardware setup.
+   */
+  ImagingHardware getHardware() {
+    return this.hardware // TODO implement deep copy
+  }
+
+  /**
+   * Returns a deep copy of the instrument's location.
+   *
+   * @see: #Location
+   * @return The instrument location.
+   */
+  Location getLocation() {
+    return this.location // TODO implement deep copy
+  }
+
 }
