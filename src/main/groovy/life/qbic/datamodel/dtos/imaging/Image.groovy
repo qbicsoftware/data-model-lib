d4fe59d97da69edc6bd52b727e0927caf28d5d33
Sven F <sven.fillinger@mailbox.org>
2020-08-14 09:56:10 +0200

    Add dto for the image object (#47)
    This CL introduces a data transfer object for an microscopy or 
    tomography image object in the context of image analysis.
    
    Co-authored-by: jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>

--- src/main/groovy/life/qbic/datamodel/dtos/imaging/Image.groovy
+++ src/main/groovy/life/qbic/datamodel/dtos/imaging/Image.groovy
@@ -1,12 +1,129 @@
 package life.qbic.datamodel.dtos.imaging
 
+import life.qbic.datamodel.dtos.imaging.parameters.AcquisitionParameters
+import life.qbic.datamodel.dtos.imaging.parameters.AnalysisParameters
+import life.qbic.datamodel.dtos.imaging.properties.ImageSize
+import life.qbic.datamodel.dtos.imaging.properties.Modality
+import life.qbic.datamodel.dtos.imaging.properties.PixelSize
+import life.qbic.datamodel.dtos.imaging.properties.Resolution
+
 /**
- * <class short description - 1 Line!>
+ * This DTO class describes an image resulted from imaging experiments
  *
- * <More detailed description - When to use, what it solves, etc.>
+ * An image class shall be used to describe properties of an image
+ * in the context of imaging experiments such as microscopy or tomography.
  *
  * @author Sven Fillinger
- * @since <versiontag>
+ * @since 1.10.0
  */
-class Image {
+final class Image {
+  /**
+   * The image acquisition date.
+   *
+   * @see Date
+   */
+  private final Date acquisitionDate
+
+  /**
+   * Detailed description of image acquisition parameters.
+   *
+   * @see AcquisitionParameters
+   */
+  private final AcquisitionParameters acquisitionParameters
+
+  /**
+   * Detailed description of image analysis parameters
+   *
+   * @see AnalysisParameters
+   */
+  private final AnalysisParameters analysisParameters
+
+  /**
+   * A detailed instrument description that was used to create the image.
+   *
+   * @see Instrument
+   */
+  private final Instrument instrument
+
+  /**
+   * The image identifier in OMERO.
+   */
+  private final Integer imageId
+
+  /**
+   * The image modality.
+   *
+   * @see Modality
+   */
+  private final Modality modality
+
+  /**
+   * Information about the image size.
+   *
+   * @see ImageSize
+   */
+  private final ImageSize imageSize
+
+  /**
+   * Information about the time and spacial distribution of
+   * a pixel.
+   *
+   * @see PixelSize
+   */
+  private final PixelSize pixelSize
+
+  /**
+   * Describes the resolution of the image.
+   *
+   * @see Resolution
+   */
+  private final Resolution resolution
+
+  /**
+   * Returns detailed image acquisition parameters.
+   * @return Image acquisition parameters.
+   */
+  AcquisitionParameters getAcquisionParameters() {
+    return this.acquisitionParameters // TODO return deep copy
+  }
+
+  /**
+   * Returns detailed image analysis parameters.
+   * @return Image analysis parameters.
+   */
+  AnalysisParameters getAnalysisParameters() {
+    return this.analysisParameters // TODO return deep copy
+  }
+
+  /**
+   * Returns detailed instrument information.
+   * @return Image instrument information.
+   */
+  Instrument getInstrument(){
+    return this.instrument // TODO return deep copy
+  }
+
+  /**
+   * Returns the image size of an image.
+   * @return The actual image size.
+   */
+  ImageSize getImageSize(){
+    return this.imageSize // TODO return deep copy
+  }
+
+  /**
+   * Returns the pixel size.
+   * @return The actual pixel size.
+   */
+  PixelSize getPixelSize(){
+    return this.pixelSize // TODO return deep copy
+  }
+
+  /**
+   * Returns image resolution information.
+   * @return The image resolution.
+   */
+  Resolution getResolution(){
+    return this.resolution // TODO return deep copy
+  }
 }
