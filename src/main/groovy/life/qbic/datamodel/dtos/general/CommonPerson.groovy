2cc80cdd741a0f393693ca55deea34bb257821cd
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-11-10 09:47:12 +0100

    Provide CommonPerson implementation

---
+++ src/main/groovy/life/qbic/datamodel/dtos/general/CommonPerson.groovy
@@ -0,0 +1,33 @@
+package life.qbic.datamodel.dtos.general
+
+/**
+ * <class short description - 1 Line!>
+ *
+ * <More detailed description - When to use, what it solves, etc.>
+ *
+ * @author Sven Fillinger
+ * @since <versiontag>
+ */
+class CommonPerson extends Person {
+
+  static class Builder extends Person.Builder<Builder> {
+
+    Builder(String firstName, String lastName, String emailAddress) {
+      super(firstName, lastName, emailAddress)
+    }
+
+    @Override
+    Person build() {
+      return new CommonPerson(this)
+    }
+
+    @Override
+    protected Builder self() {
+      return this
+    }
+  }
+
+  CommonPerson(Builder builder) {
+    super(builder)
+  }
+}
