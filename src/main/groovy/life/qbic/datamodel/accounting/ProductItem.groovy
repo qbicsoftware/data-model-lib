d1dd0657ddf51e2bb5ab5690c7a15b44588f35bb
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2020-11-16 13:29:08 +0100

    remove wrong name from class descriÃption

--- src/main/groovy/life/qbic/datamodel/accounting/ProductItem.groovy
+++ src/main/groovy/life/qbic/datamodel/accounting/ProductItem.groovy
@@ -6,7 +6,7 @@ import life.qbic.datamodel.dtos.business.services.Product
 /**
  * An item with associated costs and quantity
  *
- * Cost items describe services and their price. They are the building blocks of invoices and offers.
+ * Product items describe services and their price. They are the building blocks of invoices and offers.
  * The unit price is always provided in euros.
  * @since: 1.9.0
  * @author: Tobias Koch
