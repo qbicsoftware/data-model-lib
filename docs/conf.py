ba4d02e1e87c817ddc916e591b0c3a451c1e5a3b
JohnnyQ5 <support@qbic.zendesk.com>
2021-01-01 02:10:10 +0000

    qube sync

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'data-model-lib'
-copyright = "2020, Sven Fillinger"
+copyright = "2021, Sven Fillinger"
 author = "Sven Fillinger"
 
 # The version info for the project you're documenting, acts as replacement
