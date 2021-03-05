974436dd3fb7abedd118f9d697b3a13bde06e7ea
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-03-05 08:58:37 +0100

    Provide project objects

---
+++ src/main/groovy/life/qbic/datamodel/dtos/business/ProjectCode.groovy
@@ -0,0 +1,39 @@
+package life.qbic.datamodel.dtos.business
+
+/**
+ * Describes the project code, that identifies the project within a project space.
+ *
+ * A project code is the unique identifier of a QBiC project within a project space.
+ *
+ * A project code can be validated with the following regular expression: "Q[A-X0-9]{4}".
+ *
+ * @since 1.0.0
+ */
+class ProjectCode {
+
+    final String code
+
+    private static final def REGEX = ~'Q[A-X0-9]{4}'
+
+    /**
+     * Constructs a project code instance based on the given code String.
+     * @param code The project code
+     * @throws IllegalArgumentException If the code parameter does not match format requirements.
+     */
+    ProjectCode(String code) throws IllegalArgumentException {
+        Objects.requireNonNull(code, "Code must not be null")
+        this.code = code.trim()
+        validateCode()
+    }
+
+    private void validateCode() {
+        if(! REGEX.matcher(code).matches()) {
+            throw new IllegalArgumentException("${code} is not a valid project code.")
+        }
+    }
+
+    @Override
+    String toString() {
+        return this.code
+    }
+}
