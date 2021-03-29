69cd2cff427c495494700c04a34b2ba158a6c3b2
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-03-29 12:24:19 +0200

    Add UNKNOWN product category
    Unknown category is used for all products that could not be matched to another category. It is the default value if no assignment could be made.
    Do not use to categorize products intentionally.

--- src/main/groovy/life/qbic/datamodel/dtos/business/ProductCategory.groovy
+++ src/main/groovy/life/qbic/datamodel/dtos/business/ProductCategory.groovy
@@ -5,7 +5,7 @@ package life.qbic.datamodel.dtos.business
  *
  * A package can be of different types, they can be differentiated by the items that they contain
  *
- * @since: 1.0
+ * @since 1.0
  */
 enum ProductCategory {
 
@@ -15,7 +15,15 @@ enum ProductCategory {
     SECONDARY_BIOINFO("Secondary Bioinformatics"),
     DATA_STORAGE("Data Storage"),
     PROTEOMIC("Proteomics"),
-    METABOLOMIC("Metabolomics")
+    METABOLOMIC("Metabolomics"),
+    /**
+     * Unknown category is used for all products that could not be matched to
+     * another category. It is the default value if no assignment could be made.
+     * <p><b><em>Do not use to categorize products intentionally.</em></b></p>
+     * @since 2.5.0
+     */
+    UNKNOWN("Unknown")
+
 
     /**
      * Value describing the enum type with a string
