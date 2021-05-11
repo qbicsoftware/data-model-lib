347f40916f0f9e50325f8f9cf02bbfea8c2e04e1
jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>
2021-05-11 11:37:18 +0200

    Update src/main/groovy/life/qbic/datamodel/datasets/datastructure/files/nfcore/RunId.groovy
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>

--- src/main/groovy/life/qbic/datamodel/datasets/datastructure/files/nfcore/RunId.groovy
+++ src/main/groovy/life/qbic/datamodel/datasets/datastructure/files/nfcore/RunId.groovy
@@ -24,7 +24,8 @@ class RunId extends DataFile {
      * Creates the RunId object based on a RunId name and a relative path of it
      * @param name The file name of the RunId
      * @param relativePath The relative path to the file in a file system
-     * @return the RunId object, which also validates if the the filename is valid
+     * @return the RunId object, which also validates if the filename is valid
+     * @since 2.6.0
      */
     static RunId create(String name, String relativePath) {
         return new RunId(name, relativePath)
