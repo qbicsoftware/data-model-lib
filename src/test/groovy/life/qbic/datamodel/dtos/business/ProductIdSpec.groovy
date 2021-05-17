d113b8ddac1cba40afad9ff6db2da2cb34beea4d
Steffengreiner <Steffen.Greiner@gmx.de>
2021-05-17 09:28:22 +0200

    Add wrongly removed equality test for productIDSpec

--- src/test/groovy/life/qbic/datamodel/dtos/business/ProductIdSpec.groovy
+++ src/test/groovy/life/qbic/datamodel/dtos/business/ProductIdSpec.groovy
@@ -44,4 +44,16 @@ class ProductIdSpec extends Specification {
         "test" | "1234"
         "test2" | "0"
     }
+
+    def "Ids with different content should return false on equality comparison"() {
+        given: "Two ids with different content"
+        def productIdA = new ProductId("A", "1")
+        def productIdB = new ProductId("B", "1")
+
+        when: "we compare them"
+        def equal = productIdA == productIdB
+
+        then: "must be false"
+        assert !equal
+    }
 }
