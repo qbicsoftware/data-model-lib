94939d4ac1a3741c443a231e26c8b3c2f0f016ae
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-09-03 09:58:14 +0200

    Add tests

---
+++ src/test/groovy/life/qbic/datamodel/validation/projectmanagement/ProjectCodeValidatorSpec.groovy
@@ -0,0 +1,34 @@
+package life.qbic.datamodel.validation.projectmanagement
+
+import life.qbic.datamodel.validation.ValidationException
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * <p>Tests the ProjectCodeValidator</p>
+ *
+ * @since 2.12.0
+ */
+class ProjectCodeValidatorSpec extends Specification {
+    ProjectCodeValidator validator = new ProjectCodeValidator()
+
+    @Unroll
+    def "Accept throws no exception for valid project code: #validProjectCode"() {
+        when:
+        validator.accept(validProjectCode)
+        then:
+        noExceptionThrown()
+        where:
+        validProjectCode << ["QABCD", "QBBBA", "QPPEO", "QQQQQ"]
+    }
+
+    @Unroll
+    def "Accept throws ValidationException for invalid project code: #invalidProjectCode"() {
+        when:
+        validator.accept(invalidProjectCode)
+        then:
+        thrown(ValidationException)
+        where:
+        invalidProjectCode << [null, "", "testQABCD", "QABCD0000", "YABCD", "ZABCD", "QZZBA"]
+    }
+}
