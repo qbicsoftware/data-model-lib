d0a192ac5a5c7b3df76b5b4bc0394154c070a55b
Steffengreiner <steffen.greiner@qbic.uni-tuebingen.de>
2023-02-13 14:58:33 +0100

    Introduce minimal Schema for nanopore registration  (#350)
    * Introduce Optional Files and Folders for nanopore registration with schema and json examples + tests
    
    * Adapt OxfordNanoporeExperiment and Tests to account for optional folders and file generation instead of throwing errors
    
    * Remove barcode_alignment from the list of required files in the minimal expected datastructure
    
    * Introduce new schema version so minContains can be employed to check for minimum number of files
    
    * Include example of measurementname in minimal schema
    
    * Fix typo in Optional File description
    
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>
    
    * Remove outdated comment and switch static final
    
    * Introduce NanoporeFile and NanoporeFolder Enums and dedicated isFile and isFolder check methods
    
    ---------
    
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>

---
+++ src/main/groovy/life/qbic/datamodel/instruments/OxfordNanoporeInstrumentOutputMinimal.groovy
@@ -0,0 +1,19 @@
+package life.qbic.datamodel.instruments
+
+
+/**
+ * Represents the Nanopore instrument output data structure schema.
+ *
+ * The original schema is defined in as resource and is
+ * referenced here, wrapped in a Groovy class for reference
+ * in applications that want to validate the instrument
+ * output structure against the schema.
+ */
+class OxfordNanoporeInstrumentOutputMinimal {
+
+  private static final String SCHEMA_PATH = "/schemas/nanopore-instrument-output_minimal_schema.json"
+
+  static InputStream getSchemaAsStream() {
+    return OxfordNanoporeInstrumentOutputMinimal.getResourceAsStream(SCHEMA_PATH)
+  }
+}
